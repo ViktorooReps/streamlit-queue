d0dc37601e8a283eace9d06048dfd8ebd105eb29
viktoroo <viktoroo.sch@gmail.com>
2023-03-17 16:31:39 +0300

    notifications

--- run_streamlit.py
+++ run_streamlit.py
@@ -1,9 +1,24 @@
+import threading
+from concurrent.futures import ThreadPoolExecutor, Future
+from os import environ
+
+from pandas import DataFrame
+from streamlit_option_menu import option_menu
+
 import gspread
 import pandas as pd
 import streamlit as st
 from google.oauth2.service_account import Credentials
 from gspread import Spreadsheet, Client
+from streamlit.runtime.scriptrunner import add_script_run_ctx, get_script_run_ctx
 from streamlit_autorefresh import st_autorefresh
+from trycourier import Courier
+from validate_email import validate_email
+
+
+accounts_spreadsheet = 'https://docs.google.com/spreadsheets/d/13rT_tVMi_GItPLF3gqNE9V011qRYCteAHiwGLW-No4M/edit#gid=0'
+queue_spreadsheet = 'https://docs.google.com/spreadsheets/d/13rT_tVMi_GItPLF3gqNE9V011qRYCteAHiwGLW-No4M/edit#gid=1804024586'
+time_fmt = '%Y-%m-%d %X'
 
 
 def load_data(sheets_url):
@@ -15,6 +30,29 @@ def load_data(sheets_url):
 def get_client() -> Client:
     credentials = Credentials.from_service_account_info(info=st.secrets, scopes=['https://www.googleapis.com/auth/spreadsheets'])
     return gspread.authorize(credentials)
+
+
+def maybe_send_notification(q: DataFrame):
+    if st.session_state['NOTIFY'] and not q.empty and q['Name'][0] == st.session_state['USERNAME']:
+        client = Courier(auth_token=environ['COURIER_AUTH_TOKEN'])
+        client.send_message(
+            message={
+                "to": {
+                    "email": st.session_state['EMAIL']
+                },
+                "template": "MY7YWVJR5XMKHBN48W1HSPC7K45M"
+            }
+        )
+        st.session_state['NOTIFY'] = False
+
+
+def waiter(future: Future, f):
+    f(future.result())
+
+
+def thread_context_wrapper(f, ctx, *args, **kwargs):
+    add_script_run_ctx(threading.currentThread(), ctx)
+    return f(*args, **kwargs)
 
 
 @st.cache_resource()
@@ -29,15 +67,14 @@ def get_in_queue():
         {
             'Name': [st.session_state['USERNAME']],
             'Tg': [st.session_state['TELEGRAM']],
-            'Time': [pd.Timestamp.now().strftime('%Y-%m-%d %X')]
+            'Time': [pd.Timestamp.now().strftime(time_fmt)]
         }
     )
     spreadsheet.values_append('Queue', {'valueInputOption': 'RAW'}, {'values': data.values.tolist()})
 
 
-def get_position() -> int:
-    queue = load_data('https://docs.google.com/spreadsheets/d/13rT_tVMi_GItPLF3gqNE9V011qRYCteAHiwGLW-No4M/edit#gid=1804024586')
-    return (queue['Name'] == st.session_state['USERNAME']).idxmax()  # noqa
+def get_position(q) -> int:
+    return (q['Name'] == st.session_state['USERNAME']).idxmax()  # noqa
 
 
 def pop_queue(pos: int):
@@ -61,9 +98,15 @@ def format_interval(x):
     return f'{seconds}s'
 
 
+executor = ThreadPoolExecutor(max_workers=3)
+
+
 if __name__ == '__main__':
-    st_autorefresh(interval=30000, limit=100, key="chatgpt-queue")
-    queue_spreadsheet_key = '13rT_tVMi_GItPLF3gqNE9V011qRYCteAHiwGLW-No4M'
+    st_autorefresh(interval=30000, limit=100, key="chatgpt-queue-refresh-counter")
+    thread_ctx = get_script_run_ctx()
+    future_accounts = executor.submit(thread_context_wrapper, load_data, thread_ctx, accounts_spreadsheet)
+    future_queue = executor.submit(thread_context_wrapper, load_data, thread_ctx, queue_spreadsheet)
+    future_cached_client = executor.submit(thread_context_wrapper, get_client, thread_ctx)
 
     if 'LOGGED_IN' not in st.session_state:
         st.session_state['LOGGED_IN'] = False
@@ -71,16 +114,21 @@ if __name__ == '__main__':
     if 'USERNAME' not in st.session_state:
         st.session_state['USERNAME'] = 'Anonim'
 
-    logged_in = False
+    if 'NOTIFY' not in st.session_state:
+        st.session_state['NOTIFY'] = False
+
+    executor.submit(waiter, future_queue, maybe_send_notification)
+
     del_login = st.empty()
     if not st.session_state['LOGGED_IN']:
-        df = load_data('https://docs.google.com/spreadsheets/d/13rT_tVMi_GItPLF3gqNE9V011qRYCteAHiwGLW-No4M/edit#gid=0')
+        accounts = future_accounts.result()
 
-        names, tg = df['Name'], df['Tg']
+        names, tg = list(accounts['Name']), list(accounts['Tg'])
         names_to_tg = dict(zip(names, tg))
 
         with del_login.form("Login Form"):
-            option = st.selectbox('Who are you?', ['Anonim'] + list(names_to_tg.keys()))
+            form_names = ['Anonim'] + names
+            option = st.selectbox('Who are you?', form_names, index=form_names.index(st.session_state['USERNAME']))
 
             st.markdown("###")
             login_submit_button = st.form_submit_button(label='Login')
@@ -99,29 +147,75 @@ if __name__ == '__main__':
                     st.error("Invalid Username!")
 
     if st.session_state['LOGGED_IN']:
-        df = load_data('https://docs.google.com/spreadsheets/d/13rT_tVMi_GItPLF3gqNE9V011qRYCteAHiwGLW-No4M/edit#gid=1804024586')
-        df['In queue'] = (pd.Timestamp.now() - pd.to_datetime(df['Time'], format='%Y-%m-%d %X')).apply(format_interval).astype(str)
-        df = df.drop(['Time'], axis=1)
-        st.table(df)
+        selected_tab = option_menu(
+            None, ["Queue", "Notify", 'Settings'],
+            icons=['house', 'envelope', 'gear'],
+            menu_icon="cast",
+            default_index=0,
+            orientation="horizontal"
+        )
 
-        if not df.empty:
-            mask = (df['Name'] == st.session_state['USERNAME'])
-            in_queue = mask.any() # noqa
-        else:
-            in_queue = False
+        if selected_tab == 'Queue':
+            queue = future_queue.result()
+            queue['In queue'] = (pd.Timestamp.now() - pd.to_datetime(queue['Time'], format=time_fmt)).apply(format_interval).astype(str)
+            queue = queue.drop(['Time'], axis=1)
+            st.table(queue)
 
-        col1, col2 = st.columns(2, gap='large')
-
-        with col1:
-            if not in_queue:
-                if st.button('Get in queue', use_container_width=True):
-                    get_in_queue()
-                    st.experimental_rerun()
+            if not queue.empty:
+                mask = (queue['Name'] == st.session_state['USERNAME'])
+                in_queue = mask.any() # noqa
             else:
-                if st.button('Get out', use_container_width=True):
-                    pop_queue(get_position())
+                in_queue = False
+
+            col1, col2 = st.columns(2, gap='large')
+
+            with col1:
+                if not in_queue:
+                    if st.button('Get in queue', use_container_width=True):
+                        future_cached_client.result()
+                        get_in_queue()
+                        st.experimental_rerun()
+                else:
+                    if st.button('Get out', use_container_width=True):
+                        future_cached_client.result()
+                        pop_queue(get_position(queue))
+                        st.experimental_rerun()
+
+            with col2:
+                if st.button('Refresh', use_container_width=True):
                     st.experimental_rerun()
 
-        with col2:
-            if st.button('Refresh', use_container_width=True):
+        if selected_tab == 'Notify':
+            if 'EMAIL' in st.session_state:
+                col1, col2 = st.columns(2, gap='large')
+
+                with col1:
+                    notify = st.selectbox(
+                        'We can notify you once it is your turn',
+                        ['Do not send notifications', 'Send notifications'],
+                        index=int(st.session_state['NOTIFY'])
+                    )
+                    st.session_state['NOTIFY'] = (notify == 'Send notifications')
+
+                with col2:
+                    st.caption(f'Your email: {st.session_state["EMAIL"]}')
+
+                form_key, form_label = 'change_email', 'Change email'
+            else:
+                form_key, form_label = 'add_email', 'Add email'
+
+            with st.form(form_key):
+                email = st.text_input(form_label)
+                if st.form_submit_button('Submit'):
+                    if validate_email(email, check_format=True, check_dns=True, dns_timeout=10, check_smtp=True, smtp_timeout=10):
+                        st.session_state['EMAIL'] = email
+                        st.experimental_rerun()
+                    else:
+                        st.error(f'{email} is incorrect email address!')
+
+        if selected_tab == 'Settings':
+            if st.button('Logout'):
+                st.session_state['LOGGED_IN'] = False
                 st.experimental_rerun()
+
+
